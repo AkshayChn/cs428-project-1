1ad113a66eb3813983613ddfff0ffadae08e8b00
Akshay C <chnakshay@gmail.com>
2021-09-18 14:34:00 -0500

    Sound added ambient

---
+++ Assets/ambientSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambientSound : MonoBehaviour
+{
+    
+    public AudioSource ambient;
+    public AudioSource chatter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambient.Play();
+        chatter.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
